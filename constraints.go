f18f4721b4e9100c96916c050db7b5296eda795d
Billy <birwin93@gmail.com>
2021-12-18 11:41:19 -0500

    Updates fn.Num to use constraints

--- constraints.go
+++ constraints.go
@@ -1,5 +1,7 @@
 package fn
 
+import "constraints"
+
 type Num interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~float32 | ~float64
+	constraints.Integer | constraints.Float | constraints.Complex
 }
